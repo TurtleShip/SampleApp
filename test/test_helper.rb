742f9a43d18012d6f9954205d4cbc69caee7b095
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-09-15 23:19:53 -0700

    Add minitest reporters.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters' # Color dots green/red depending on test results
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
