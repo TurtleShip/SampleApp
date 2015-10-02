9e07e95ba0a25065719762d84a2dba7c4cae6729
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-10-02 00:33:13 -0700

    Encrypt user's password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 50},
             format: {with: VALID_EMAIL_REGEX},
-            uniqueness: { case_sensitive: false }
+            uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
+
+  has_secure_password
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com')
+    @user = User.new(name: 'Example User', email: 'user@example.com',
+                     password: 'foobar', password_confirmation: 'foobar')
   end
 
   test 'should be valid' do
@@ -61,4 +62,15 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal original_email.downcase, @user.email, 'email should be downcased after being saved'
   end
+
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
+
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
+  end
+
 end
