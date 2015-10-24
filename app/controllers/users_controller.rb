d74648f0df78840d9a39095daec3bbeede50fa08
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-10-24 16:36:08 -0700

    Make sure only authorized users can make edits.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -35,9 +37,21 @@ class UsersController < ApplicationController
 
 
   private
-  # Adding an extra level of indentation on the user_params method to make it visually
-  # apparent which methods are defined after private.
+    # Adding an extra level of indentation on the user_params method to make it visually
+    # apparent which methods are defined after private.
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url
+      end
+    end
+
+
 end
