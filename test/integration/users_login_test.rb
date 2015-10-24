2517d3a98c031c09458cc46e40266c410ade9371
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-10-24 14:45:27 -0700

    Make remembering a user optional.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -82,4 +82,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not is_logged_in?
   end
 
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test 'login without remembering' do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+
 end
