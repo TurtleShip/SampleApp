32fb6068512730b3d780998624853e427f642f8a
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-10-24 22:25:23 -0700

    Add more tests on site layout.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,17 +2,37 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
-    get root_path
-    assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
+  def setup
+    @user = users(:michael)
   end
 
-  test "signup page should have correct title" do
+  test 'layout links when a user is not logged in' do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', signup_path
+    assert_select 'a[href=?]', login_path
+  end
+
+  test 'layout links when a user is logged in' do
+    log_in_as @user
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', login_path, false
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+  end
+
+  test 'signup page should have correct title' do
     get signup_path
     assert_template 'users/new'
     assert_select 'title', full_title('Sign up')
