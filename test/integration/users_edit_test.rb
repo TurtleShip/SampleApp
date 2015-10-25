f338b66de26efe4e23eb4b688141188c6837431d
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-10-24 22:16:40 -0700

    Make sure forwarding url gets reset after following it.

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -19,6 +19,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test 'successful edit with friendly forwarding' do
     get edit_user_path(@user)
+    assert_not_nil session[:forwarding_url]
     log_in_as @user
     assert_redirected_to edit_user_path(@user)
     name = 'Seulgi Two'
@@ -31,6 +32,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     # Flash should contain a success message
     assert_not flash.empty?
     assert_redirected_to @user
+    assert_nil session[:forwarding_url]
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
