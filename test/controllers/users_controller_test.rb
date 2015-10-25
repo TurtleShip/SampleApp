d75ce8a2fa0196c0baa532b33972a4501866441b
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-10-24 21:36:31 -0700

    Allow admins to delete users.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
 
   def setup
-    @user = users(:seulgi)
+    @user = users(:michael)
     @other_user = users(:soyeon)
   end
 
@@ -41,6 +41,21 @@ class UsersControllerTest < ActionController::TestCase
     log_in_as(@other_user)
     patch :update, id: @user, user: {name: @user.name, email: @user.email}
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when logged in as a non-admin' do
+    assert_no_difference 'User.count' do
+      log_in_as @user
+      delete :destroy, id: @other_user
+    end
     assert_redirected_to root_url
   end
 
